1e2bb2fd8f44fd95457125e6150535702b2ce4e3
Michael Waskom <mwaskom@stanford.edu>
2011-10-19 15:21:51 -0700

    Previous fix introduced bug.  Now killed

--- workflows/preproc.py
+++ workflows/preproc.py
@@ -604,7 +604,8 @@ def write_realign_report(realign_params, rms_files):
     # Open up the RMS files and get the max motion
     displace = map(np.loadtxt, rms_files)
     motion = map(max, displace)
-    motion.append(sum(displace[1]))
+    motion.append(displace[1].sum())
+    displace[1] = np.concatenate(([0], displace[1]))
     motion_file = abspath("max_motion.txt")
     with open(motion_file, "w") as f:
        f.write("#Absolute:\n%.4f\n#Relative\n%.4f\nTotal\n%.4f"%tuple(motion))
