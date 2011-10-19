7fac34498f7b0408dd35a7c5b1fa6b516ea2168d
Michael Waskom <mwaskom@stanford.edu>
2011-10-19 15:09:50 -0700

    Another shot at the realign report bug

--- workflows/preproc.py
+++ workflows/preproc.py
@@ -604,7 +604,8 @@ def write_realign_report(realign_params, rms_files):
     # Open up the RMS files and get the max motion
     displace = map(np.loadtxt, rms_files)
     motion = map(max, displace)
-    motion.extend(displace[1].sum())
+    rel_motion = displace[1]
+    motion.extend(rel_motion.sum())
     displace[1] = np.concatenate(([0], displace[1]))
     motion_file = abspath("max_motion.txt")
     with open(motion_file, "w") as f:
