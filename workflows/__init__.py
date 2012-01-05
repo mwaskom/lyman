309ce9dc167d72b9b2e603fa03a86ca621cbacc8
Michael Waskom <mwaskom@stanford.edu>
2012-01-04 17:14:15 -0800

    Renaming fslmodel => model

--- workflows/__init__.py
+++ workflows/__init__.py
@@ -1,4 +1,4 @@
 from preproc import create_preprocessing_workflow
-from fslmodel import create_timeseries_model_workflow
+from model import create_timeseries_model_workflow
 from registration import (create_mni_reg_workflow,
                           create_epi_reg_workflow)
