a9876751db65fb76b429304b13aacb89f4f4ec89
Michael Waskom <mwaskom@stanford.edu>
2012-01-16 11:32:12 -0800

    Initial mixedfx report implementation

--- workflows/mixedfx.py
+++ workflows/mixedfx.py
@@ -1,15 +1,17 @@
 from nipype.interfaces import fsl
-from nipype.interfaces.utility import IdentityInterface
+from nipype.interfaces.utility import IdentityInterface, Function
 from nipype.pipeline.engine import Node, MapNode, Workflow
 
 
 def create_volume_mixedfx_workflow(name="volume_group",
+                                   subject_list=[],
                                    regressors=[], contrasts=[],
                                    flame_mode="flame1",
                                    overlay_thresh=(2.3, 4.265),
                                    cluster_zthresh=2.3, grf_pthresh=0.05):
 
-    inputnode = Node(IdentityInterface(fields=["copes",
+    inputnode = Node(IdentityInterface(fields=["l1_contrast",
+                                               "copes",
                                                "varcopes",
                                                "dofs"]),
                      name="inputnode")
@@ -57,6 +59,17 @@ def create_volume_mixedfx_workflow(name="volume_group",
                      name="slicer")
     slicer.inputs.sample_axial = 2
 
+    # Build pdf and html reports
+    report = Node(Function(input_names=["subject_list",
+                                        "l1_contrast",
+                                        "zstat_pngs",
+                                        "contrasts"],
+                              output_names=["reports"],
+                              function=write_mfx_report),
+                     name="report")
+    report.inputs.subject_list = subject_list
+    report.inputs.contrasts = contrasts
+
     outputnode = Node(IdentityInterface(fields=["flameo_stats",
                                                 "thresh_zstat",
                                                 "cluster_image",
@@ -96,6 +109,8 @@ def create_volume_mixedfx_workflow(name="volume_group",
             [("out_file", "in_file")]),
         (slicer, outputnode,
             [("out_file", "stat_png")]),
+        (slicer, report,
+            [("out_file", "zstat_pngs")]),
         (cluster, outputnode,
             [("threshold_file", "thresh_zstat"),
              ("index_file", "cluster_image"),
@@ -105,3 +120,34 @@ def create_volume_mixedfx_workflow(name="volume_group",
         ])
 
     return group_anal, inputnode, outputnode
+
+
+def write_mfx_report(subject_list, l1_contrast, zstat_pngs, contrasts):
+    import time
+    from tools import write_workflow_report
+    from workflows.reporting import mfx_report_template
+
+    # Fill in the initial report template dict
+    report_dict = dict(now=time.asctime(),
+                       l1_contrast=l1_contrast,
+                       subject_list_=", ".join(subject_list),
+                       n_subs=len(subject_list))
+
+    # Add the zstat image templates and update the dict
+    for i, con in enumerate([c[0] for c in contrasts], 1):
+        report_dict["con%d_name" % i] = con
+        report_dict["zstat%d_png" % i] = zstat_pngs[i - 1]
+        header = "Zstat %d: %s" % (i, con)
+        mfx_report_template = "\n".join(
+            [mfx_report_template,
+             header,
+             "".join(["^" for l in header]),
+             "",
+             "".join([".. image:: %(zstat", str(i), "_png)s"]),
+             "    :width: 6.5in",
+             ""])
+
+    out_files = write_workflow_report("mfx",
+                                      mfx_report_template,
+                                      report_dict)
+    return out_files
