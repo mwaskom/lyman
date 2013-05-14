a5493b4e6232ef03abcc31cb911adc01980392a9
Michael Waskom <mwaskom@stanford.edu>
2013-05-13 18:17:42 -0700

    Yet another upsampling bug fix

--- lyman/mvpa.py
+++ lyman/mvpa.py
@@ -222,7 +222,7 @@ def extract_dataset(sched, timeseries, mask, tr=2, frames=None,
         time_points = len(roi_data)
         x = np.linspace(0, time_points - 1, time_points)
         xx = np.linspace(0, time_points - 1,
-                         (time_points - 1) * upsample + 1)
+                         time_points + (upsample - 1) * (upsample - 1))
         interpolator = sp.interpolate.interp1d(x, roi_data, "cubic", axis=0)
         roi_data = interpolator(xx)
 
