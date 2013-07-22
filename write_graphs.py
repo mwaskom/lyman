3eb4c29b0c8041cb62e76b141c8f9d8334aa7038
Michael Waskom <mwaskom@stanford.edu>
2013-07-22 14:15:16 -0700

    Adding script to generate workflow graphs, with the resulting images

---
+++ write_graphs.py
@@ -0,0 +1,38 @@
+"""Save graphical representations of all the lyman workflows."""
+import os
+import re
+from glob import glob
+from lyman import workflows as wf
+
+
+def main():
+
+    # Find the functions that create workflows
+    wf_funcs = [k for k in wf.__dict__ if
+                    re.match("create_.*_workflow", k)]
+
+    for func in wf_funcs:
+        try:
+            out = getattr(wf, func)()
+        except:
+            print "ERROR: call to %s failed" % func
+
+        # Some of the workflow functions return (flow, inputs, outputs)
+        try:
+            flow, _, _ = out
+        except TypeError:
+            flow = out
+
+        # Write the graphs
+        name = flow.name
+        flow.write_graph("graphs/%s.dot" % name, "orig")
+
+    # Remove the .dot files as they are not of use to us
+    files = glob("graphs/*")
+    for f in files:
+        if f.endswith(".dot"):
+            os.remove(f)
+
+
+if __name__ == "__main__":
+    main()
