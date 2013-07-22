ee07e99faa1fbc510c46f31f8d910aed9de30945
Michael Waskom <mwaskom@stanford.edu>
2013-07-22 14:14:37 -0700

    Starting to flatten package with graph utility functions

--- lyman/tools/tests/test_main.py
+++ lyman/tools/tests/test_main.py
@@ -1,84 +1,9 @@
 import numpy as np
 import pandas as pd
 from argparse import Namespace
-from nipype.testing import assert_equal, assert_true
-
-from nipype.pipeline.engine import Workflow, Node, MapNode
-from nipype.interfaces.utility import IdentityInterface
-from nipype.interfaces.io import DataGrabber
+from nipype.testing import assert_equal
 
 from .. import main
-
-
-def make_simple_workflow():
-
-    wf = Workflow(name="test")
-
-    node1 = Node(IdentityInterface(fields=["foo"]), name="node1")
-    node2 = MapNode(IdentityInterface(fields=["foo"]),
-                    name="node2", iterfield=["foo"])
-    node3 = Node(IdentityInterface(fields=["foo"]), name="node3")
-
-    wf.connect([
-        (node1, node2, [("foo", "foo")]),
-        (node2, node3, [("foo", "foo")]),
-        ])
-
-    return wf, node1, node2, node3
-
-
-def test_input_wrapper():
-
-    wf, node1, node2, node3 = make_simple_workflow()
-
-    s_list = ['s1', 's2']
-    s_node = main.make_subject_source(s_list)
-
-    g_node = Node(DataGrabber(in_fields=["foo"],
-                              out_fields=["bar"]),
-                  name="g_node")
-
-    iw = main.InputWrapper(wf, s_node, g_node, node1)
-
-    yield assert_equal, iw.wf, wf
-    yield assert_equal, iw.subj_node, s_node
-    yield assert_equal, iw.grab_node, g_node
-    yield assert_equal, iw.in_node, node1
-
-    iw.connect_inputs()
-
-    g = wf._graph
-    yield assert_true, s_node in g.nodes()
-    yield assert_true, g_node in g.nodes()
-    yield assert_true, (s_node, g_node) in g.edges()
-
-
-def test_make_subject_source():
-
-    subj_list = ['s1', 's2', 's3']
-    node = main.make_subject_source(subj_list)
-    iterable_name, iterable_val = node.iterables
-    yield assert_equal, iterable_name, "subject_id"
-    yield assert_equal, iterable_val, subj_list
-
-
-def test_find_mapnodes():
-
-    wf = make_simple_workflow()[0]
-    mapnodes = main.find_mapnodes(wf)
-    yield assert_equal, mapnodes, ["node2"]
-
-
-def test_find_nested_workflows():
-
-    wf, node1, node2, node3 = make_simple_workflow()
-    inner_wf = make_simple_workflow()[0]
-
-    wf.connect(node3, "foo", inner_wf, "node1.foo"),
-
-    workflows = main.find_nested_workflows(wf)
-
-    yield assert_equal, workflows, [inner_wf]
 
 
 def test_determine_engine():
@@ -99,27 +24,12 @@ def test_determine_engine():
             yield assert_equal, plugin_args, dict(n_procs=4, qsub_args="")
 
 
-def test_find_contrast_number():
-
-    contrasts = ["foo", "bar", "buz"]
-
-    for i, contrast in enumerate(contrasts, 1):
-        yield assert_equal, main.find_contrast_number(contrast, contrasts), i
-
-
-def test_reg_template():
-
-    yield assert_equal, "foo", main.reg_template("_mask", "foo", "bar")
-    yield assert_equal, "qux", main.reg_template("whatever", "buz", "qux")
-
-
 def test_locate_peaks():
 
     challenge = [
         ([(60, 60, 50)], ("L Cereb WM", 95)),
         ([(62, 69, 50)], ("MFG", 20)),
         ([(31, 50, 27)], ("Parahip G, post", 30)),
-        # This is actually suboptimal, I'd eventually like this to fail
         ([(26, 55, 27)], ("Temp Fus Ctx, post", 3)),
         ([(29, 50, 30)], ("R Hippocampus", 95))]
 
