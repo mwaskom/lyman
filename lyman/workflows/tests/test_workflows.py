1524645130e1ed6a91dfad4011a3c4b8eac0164c
Michael Waskom <mwaskom@stanford.edu>
2013-08-12 21:57:15 -0700

    PEP8

--- lyman/workflows/tests/test_workflows.py
+++ lyman/workflows/tests/test_workflows.py
@@ -1,4 +1,4 @@
-"""Very basic tests the workflow factories are importable and callable."""
+"""Very basic tests that workflow factories are importable and callable."""
 import re
 from nose.tools import assert_is_instance
 from nipype import Workflow, Node, IdentityInterface
@@ -7,7 +7,7 @@ from lyman import workflows as wf
 
 
 def test_workflow_functions():
-
+    """Written as generator for clarity about which tests are failing."""
     pat = re.compile("create_(.+)_workflow")
     funcs = [getattr(wf, k) for k in dir(wf) if pat.match(k)]
 
@@ -20,8 +20,8 @@ def test_workflow_functions():
                 w, i, o = ret
             except TypeError:
                 assert_is_instance(ret, Workflow)
-                return 
-                
+                return
+
             assert_is_instance(w, Workflow)
             assert_is_instance(i, Node)
             assert_is_instance(o, Node)
