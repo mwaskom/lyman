9ece19a3326ef51ffe4666840766103e318221b6
Michael Waskom <mwaskom@stanford.edu>
2014-06-03 13:01:12 -0700

    Remove old nipy-based normalization report functions

--- lyman/workflows/anatwarp.py
+++ lyman/workflows/anatwarp.py
@@ -9,17 +9,15 @@ information about the processing.
 import os
 import os.path as op
 import numpy as np
-import scipy as sp
 import matplotlib.pyplot as plt
-import matplotlib.image as mplimg
 import nibabel as nib
-from nipy.labs import viz
 import seaborn as sns
 
-from nipype import IdentityInterface, Function, DataGrabber, DataSink
+from nipype import (IdentityInterface, Function,
+                    DataGrabber, DataSink,
+                    Node, Workflow)
 from nipype.interfaces import fsl
 from nipype.interfaces import freesurfer as fs
-from nipype import Node, Workflow
 
 
 def create_anatwarp_workflow(data_dir=None, subjects=None, name="anatwarp"):
@@ -194,80 +192,3 @@ def create_anatwarp_workflow(data_dir=None, subjects=None, name="anatwarp"):
     return normalize
 
 
-def warp_report(in_file):
-    """Plot the registration summary using nipy contours"""
-    mni_file = op.join(os.environ["FSL_DIR"],
-                       "data/standard/MNI152_T1_1mm_brain.nii.gz")
-    mni_img = nib.load(mni_file)
-    mni_data, mni_aff = mni_img.get_data(), mni_img.get_affine()
-    sub_img = nib.load(in_file)
-    sub_data, sub_aff = sub_img.get_data(), sub_img.get_affine()
-    sub_data[sub_data < 1] = 0
-
-    kwargs = dict(draw_cross=False, annotate=False)
-    cut_coords = dict(x=(-45, -12, 12, 45),
-                      y=(-55, -25, 5, 45),
-                      z=(-30, -5, 20, 40))
-
-    colors = sns.color_palette("bright")
-    im_data = dict()
-    for axis in ["x", "y", "z"]:
-        f = plt.figure(figsize=(10, 2.5))
-        coords = cut_coords[axis]
-        slicer = viz.plot_anat(sub_data, sub_aff, slicer=axis,
-                               cut_coords=coords, figure=f, **kwargs)
-        slicer.contour_map(mni_data, mni_aff, colors=colors)
-        fname = "slices_%s.png" % axis
-        f.savefig(fname, facecolor="k", edgecolor="k")
-        im_data[axis] = mplimg.imread(fname)
-
-    concat_data = [im_data[axis] for axis in ["x", "y", "z"]]
-    concat_data = np.concatenate(concat_data, axis=0)
-    mplimg.imsave("warp_report.png", concat_data)
-    return op.abspath("warp_report.png")
-
-
-def mni_reg_qc(in_file):
-    """Plot the registration using external FSL routines."""
-    import os.path as op
-    from subprocess import call
-    from nipype.interfaces import fsl
-
-    mni_targ = fsl.Info.standard_image("MNI152_T1_1mm_brain.nii.gz")
-
-    planes = ["x", "y", "z"]
-    options = []
-    for plane in planes:
-        for slice in ["%.2f" % i for i in .15, .3, .45, .5, .55, .7, .85]:
-            if not(plane == "x" and slice == "0.50"):
-                options.append((plane, slice))
-
-    shots = ["%s-%s.png" % i for i in options]
-
-    for i, shot in enumerate(shots):
-        cmd = ["slicer",
-               in_file,
-               mni_targ,
-               "-s .8",
-               "-%s" % options[i][0],
-               options[i][1],
-               shot]
-
-        call(cmd)
-
-    for i in range(3):
-        cmd = ["pngappend"]
-        cmd.append(" + ".join(
-            [s for s in shots if op.split(s)[1].startswith(planes[i])]))
-        rowimg = "row-%d.png" % i
-        cmd.append(rowimg)
-        shots.append(rowimg)
-        call(" ".join(cmd), shell=True)
-
-    cmd = ["pngappend"]
-    cmd.append(" - ".join(["row-%d.png" % i for i in range(3)]))
-    out_file = op.join(op.abspath("."), "brain_warp_to_mni.png")
-    cmd.append(out_file)
-    call(" ".join(cmd), shell=True)
-
-    return out_file
