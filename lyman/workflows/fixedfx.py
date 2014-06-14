64baff56bcfe88628ae5afbdb5814fc0e2e97f64
Michael Waskom <mwaskom@stanford.edu>
2014-06-13 18:07:58 -0700

    Move ffx report into interface

--- lyman/workflows/fixedfx.py
+++ lyman/workflows/fixedfx.py
@@ -7,8 +7,9 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import seaborn as sns
 import moss
+from moss.mosaic import Mosaic
 
-from nipype import Workflow, Node, IdentityInterface, Function
+from nipype import Workflow, Node, IdentityInterface
 from nipype.interfaces import fsl
 from nipype.interfaces.base import (BaseInterface,
                                     BaseInterfaceInputSpec,
@@ -54,20 +55,13 @@ def create_ffx_workflow(name="mni_ffx", space="mni", contrasts=None):
     ffxsummary = Node(FFXSummary(), "ffxsummary")
 
     # Plot the fixedfx results
-    report = Node(Function(["space",
-                            "anatomy",
-                            "zstat_files",
-                            "r2_files",
-                            "masks"],
-                           ["report"],
-                           fixedfx_report,
-                           imports),
-                  "report")
-    report.inputs.space = space
+    report = Node(FFXReport(space=space), "report")
 
     outputnode = Node(IdentityInterface(["flame_results",
                                          "r2_files",
-                                         "report"]),
+                                         "tsnr_file",
+                                         "summary_report",
+                                         "zstat_report"]),
                       "outputs")
 
     ffx = Workflow(name=name)
@@ -88,13 +82,16 @@ def create_ffx_workflow(name="mni_ffx", space="mni", contrasts=None):
         (ffxmodel, report,
             [("zstat_files", "zstat_files")]),
         (ffxsummary, report,
-            [("r2_files", "r2_files")]),
+            [("r2_files", "r2_files"),
+             ("tsnr_file", "tsnr_file")]),
         (ffxmodel, outputnode,
             [("flame_results", "flame_results")]),
         (ffxsummary, outputnode,
-            [("r2_files", "r2_files")]),
+            [("r2_files", "r2_files"),
+            ("tsnr_file", "tsnr_file")]),
         (report, outputnode,
-            [("report", "report")]),
+            [("summary_files", "summary_report"),
+             ("zstat_files", "zstat_report")]),
     ])
 
     return ffx, inputnode, outputnode
@@ -302,7 +299,7 @@ class FFXSummary(BaseInterface):
     def compute_tsnr(self):
         """Compute the toal signal to noise."""
         # Load the mean images from each run and take the grand mean."""
-        mean_imgs  = [nib.load(f) for f in self.inputs.means]
+        mean_imgs = [nib.load(f) for f in self.inputs.means]
         mean_data = np.mean([img.get_data() for img in mean_imgs], axis=0)
 
         # Use the timeseries to get the total number of frames
@@ -313,7 +310,7 @@ class FFXSummary(BaseInterface):
         res_sumsquares = self.sum_squares("ssres", "full")
         res_std = np.sqrt(res_sumsquares / n)
         tsnr = mean_data / res_std
-        self.save_image(tsnr, "tsnr") 
+        self.save_image(tsnr, "tsnr")
 
     def save_image(self, data, fname):
         """Save data to the output structure."""
@@ -329,122 +326,96 @@ class FFXSummary(BaseInterface):
         return outputs
 
 
-def fixedfx_report(space, anatomy, zstat_files, r2_files, masks):
-    """Plot the resulting data."""
-    sns.set()
-    bg = nib.load(anatomy).get_data()
+class FFXReportInput(BaseInterfaceInputSpec):
 
-    mask_data = [nib.load(f).get_data() for f in masks]
-    mask = np.where(np.all(mask_data, axis=0), np.nan, 1)
-    mask[bg < moss.percentiles(bg, 5)] = np.nan
+    space = traits.Either("mni", "epi")
+    anatomy = File(exists=True)
+    tsnr_file = File(exists=True)
+    zstat_files = InputMultiPath(File(exists=True))
+    r2_files = InputMultiPath(File(exists=True))
+    masks = InputMultiPath(File(exists=True))
 
-    # Find the plot parameters
-    xdata = np.flatnonzero(bg.any(axis=1).any(axis=1))
-    xslice = slice(xdata.min(), xdata.max() + 1)
-    ydata = np.flatnonzero(bg.any(axis=0).any(axis=1))
-    yslice = slice(ydata.min(), ydata.max() + 1)
-    zdata = np.flatnonzero(bg.any(axis=0).any(axis=0))
-    zmin, zmax = zdata.min(), zdata.max()
 
-    step = 2 if space == "mni" else 1
-    offset = 4 if space == "mni" else 0
+class FFXReportOutput(TraitedSpec):
 
-    n_slices = (zmax - zmin) // step
-    n_row, n_col = n_slices // 8, 8
-    start = n_slices % n_col // step + zmin + offset
-    figsize = (10, 1.375 * n_row)
-    slices = (start + np.arange(zmax - zmin))[::step][:n_slices]
-    pltkws = dict(nrows=int(n_row), ncols=int(n_col),
-                  figsize=figsize, facecolor="k")
-    pngkws = dict(dpi=100, bbox_inches="tight", facecolor="k", edgecolor="k")
+    summary_files = OutputMultiPath(File(exists=True))
+    zstat_files = OutputMultiPath(Directory(exists=True))
 
-    vmin, vmax = 0, moss.percentiles(bg, 95)
-    mask_cmap = mpl.colors.ListedColormap(["#160016"])
 
-    report = []
+class FFXReport(BaseInterface):
 
-    def add_colorbar(f, cmap, low, high, left, width, fmt):
-        cbar = np.outer(np.arange(0, 1, .01), np.ones(10))
-        cbar_ax = f.add_axes([left, 0, width, .03])
-        cbar_ax.imshow(cbar.T, aspect="auto", cmap=cmap)
-        cbar_ax.axis("off")
-        f.text(left - .01, .018, fmt % low, ha="right", va="center",
-               color="white", size=13, weight="demibold")
-        f.text(left + width + .01, .018, fmt % high, ha="left",
-               va="center", color="white", size=13, weight="demibold")
+    input_spec = FFXReportInput
+    output_spec = FFXReportOutput
 
-    # Plot the mask edges
-    f, axes = plt.subplots(**pltkws)
-    mask_colors = sns.husl_palette(len(mask_data))
-    mask_colors.reverse()
-    cmaps = [mpl.colors.ListedColormap([c]) for c in mask_colors]
-    for i, ax in zip(slices, axes.ravel()):
-        ax.imshow(bg[xslice, yslice, i].T,
-                  cmap="gray", vmin=vmin, vmax=vmax)
-        for j, m in enumerate(mask_data):
-            if m[xslice, yslice, i].any():
-                ax.contour(m[xslice, yslice, i].T,
-                           cmap=cmaps[j], linewidths=.75)
-        ax.axis("off")
-    text_min = max(.15, .5 - len(mask_data) * .05)
-    text_max = min(.85, .5 + len(mask_data) * .05)
-    text_pos = np.linspace(text_min, text_max, len(mask_data))
-    for i, color in enumerate(mask_colors):
-        f.text(text_pos[i], .03, "Run %d" % (i + 1), color=color,
-               size=11, weight="demibold", ha="center", va="center")
-    mask_png = op.abspath("mask_overlap.png")
-    plt.savefig(mask_png, **pngkws)
-    report.append(mask_png)
-    plt.close(f)
+    def _run_interface(self, runtime):
 
-    # Now plot the R2 images
-    for fname, cmap in zip(r2_files, ["GnBu_r", "YlGn_r"]):
-        f, axes = plt.subplots(**pltkws)
-        r2data = nib.load(fname).get_data()
-        rmax = r2data[~np.isnan(r2data)].max()
-        r2data[bg == 0] = np.nan
-        for i, ax in zip(slices, axes.ravel()):
-            ax.imshow(bg[xslice, yslice, i].T,
-                      cmap="gray", vmin=vmin, vmax=vmax)
-            ax.imshow(r2data[xslice, yslice, i].T, cmap=cmap,
-                      vmin=0, vmax=rmax, alpha=.8)
-            ax.imshow(mask[xslice, yslice, i].T, alpha=.5,
-                      cmap=mask_cmap, interpolation="nearest")
-            ax.axis("off")
-        savename = op.abspath(op.basename(fname).replace(".nii.gz", ".png"))
-        add_colorbar(f, cmap, 0, rmax, .35, .3, "%.2f")
-        plt.savefig(savename, **pngkws)
-        report.append(savename)
-        plt.close(f)
+        self.step = 1 if self.inputs.space == "epi" else 2
+        self.summary_files = []
+        self.zstat_files = []
+        self.write_mask_image()
+        self.write_tsnr_image()
+        self.write_r2_images()
+        self.write_zstat_images()
 
-    # Finally plot each zstat image
-    for fname in zstat_files:
-        zdata = nib.load(fname).get_data()
-        zpos = zdata.copy()
-        zneg = zdata.copy()
-        zpos[zdata < 2.3] = np.nan
-        zneg[zdata > -2.3] = np.nan
-        zlow = 2.3
-        zhigh = max(np.abs(zdata).max(), 3.71)
-        f, axes = plt.subplots(**pltkws)
-        for i, ax in zip(slices, axes.ravel()):
-            ax.imshow(bg[xslice, yslice, i].T, cmap="gray",
-                      vmin=vmin, vmax=vmax)
-            ax.imshow(zpos[xslice, yslice, i].T, cmap="Reds_r",
-                      vmin=zlow, vmax=zhigh)
-            ax.imshow(zneg[xslice, yslice, i].T, cmap="Blues",
-                      vmin=-zhigh, vmax=-zlow)
-            ax.imshow(mask[xslice, yslice, i].T, alpha=.5,
-                      cmap=mask_cmap, interpolation="nearest")
-            ax.axis("off")
-        add_colorbar(f, "Blues", -zhigh, -zlow, .18, .23, "%.1f")
-        add_colorbar(f, "Reds_r", zlow, zhigh, .59, .23, "%.1f")
+        return runtime
 
-        contrast = fname.split("/")[-2]
-        os.mkdir(contrast)
-        savename = op.join(contrast, "zstat1.png")
-        f.savefig(savename, **pngkws)
-        report.append(op.abspath(contrast))
-        plt.close(f)
+    def write_mask_image(self):
+        """Show the overlap of each run's mask."""
+        mask_imgs = [nib.load(f) for f in self.inputs.masks]
+        mask_data = [img.get_data()[..., np.newaxis] for img in mask_imgs]
+        each_mask = np.concatenate(mask_data, axis=-1)
+        any_mask = each_mask.max(axis=-1)
+        self.every_mask = each_mask.min(axis=-1)
 
-    return report
+        m = Mosaic(self.inputs.anatomy, each_mask, any_mask,
+                   stat_interp="nearest", step=self.step, show_mask=False)
+        m.plot_mask_edges()
+        out_fname = op.abspath("mask_overlap.png")
+        self.summary_files.append(out_fname)
+        m.savefig(out_fname)
+        m.close()
+
+    def write_tsnr_image(self):
+        """Show the total subject SNR."""
+        m = Mosaic(self.inputs.anatomy,
+                   self.inputs.tsnr_file,
+                   self.every_mask,
+                   step=self.step)
+        m.plot_overlay("cube:1.9:.5", 0, alpha=1, fmt="%d")
+        out_fname = op.abspath("tsnr.png")
+        self.summary_files.append(out_fname)
+        m.savefig(out_fname)
+        m.close()
+
+    def write_r2_images(self):
+        """Show the fixed effects model fit."""
+        cmaps = ["cube:2:0", "cube:2.6:0"]
+        for fname, cmap in zip(self.inputs.r2_files, cmaps):
+            m = Mosaic(self.inputs.anatomy, fname,
+                       self.every_mask, step=self.step)
+            m.plot_overlay(cmap, 0, alpha=.6)
+            out_fname = op.abspath(nii_to_png(fname))
+            self.summary_files.append(out_fname)
+            m.savefig(out_fname)
+            m.close()
+
+    def write_zstat_images(self):
+        """Show the fixed effects inference maps."""
+        for fname in self.inputs.zstat_files:
+            m = Mosaic(self.inputs.anatomy, fname,
+                       self.every_mask, step=self.step)
+            m.plot_activation(pos_cmap="Reds_r", neg_cmap="Blues",
+                              thresh=2.3, alpha=.8)
+            contrast = fname.split("/")[-2]
+            os.mkdir(contrast)
+            out_fname = op.join(contrast, "zstat1.png")
+            self.zstat_files.append(op.abspath(contrast))
+            m.savefig(out_fname)
+            m.close()
+
+    def _list_outputs(self):
+
+        outputs = self._outputs().get()
+        outputs["summary_files"] = self.summary_files
+        outputs["zstat_files"] = self.zstat_files
+        return outputs
