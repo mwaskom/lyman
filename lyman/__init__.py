39f55274163ddc02729acdbff4b2438017ffd138
Michael Waskom <mwaskom@nyu.edu>
2017-10-09 18:48:16 -0400

    Import library modules into top-level namespace

--- lyman/__init__.py
+++ lyman/__init__.py
@@ -1,4 +1,10 @@
 from .frontend import *  # flake8: noqa
+from . import glm  # flake8: noqa
+from . import utils # flake8: noqa
+from . import surface  # flake8: noqa
+from . import signals  # flake8: noqa
+from . import workflows  # flake8: noqa
+from . import visualizations  # flake8: noqa
 
 version = "2.0.0.dev"
 __version__ = version
